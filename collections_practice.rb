6dad17a9268e54145d05ab746e24c422bb90eb0d
Sergey Zolotukhin <zolotukhinsereg@gmail.com>
2020-04-03 02:12:23 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,41 @@
+def sort_array_asc(array)
+  array.sort
+end
 
+def sort_array_desc(array)
+  array.sort {|x, y| y <=> x}
+end
+
+def sort_array_char_count(array)
+  array.sort {|x, y| x.length <=> y.length}
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+  
+def kesha_maker(array)
+  array.each {|i| i[2] = "$"}
+end
+
+def find_a(array)
+  array.select {|i| i.start_with?("a")}
+end
+
+def sum_array(array)
+  array.inject(:+)
+end
+
+def add_s(array)
+  array.each_with_index.collect {|element, index| element = index == 1 ? element : element << "s"}
+end
+
+def swap_elements_from_to(array, index, destination_index)
+  array[index], array[destination_index] = array[destination_index], array[index]
+  array
+end
